d60c30f31a2dabdfe0270045611508923fbacde0
Rodrigo Bighetti <robighetti@gmail.com>
2024-04-04 16:33:22 -0300

    criaçao do toggle e menus

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,8 @@
 import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
 
+import { AccountMenu } from './account-menu'
 import { NavLink } from './nav-link'
+import { ThemeToggle } from './themes/theme-toggle'
 import { Separator } from './ui/separator'
 
 export function Header() {
@@ -22,6 +24,10 @@ export function Header() {
             Pedidos
           </NavLink>
         </nav>
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggle />
+          <AccountMenu />
+        </div>
       </div>
     </div>
   )
